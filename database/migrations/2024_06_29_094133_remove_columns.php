d1d3087cc1e0115402b598ae9d54f914897df97c
Harshit Bisen <harshitbisen@Harshits-MacBook-Air.local>
2024-07-09 10:35:46 +0530

    migration_code_uncomment

--- database/migrations/2024_06_29_094133_remove_columns.php
+++ database/migrations/2024_06_29_094133_remove_columns.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         //
-        Schema::table('users',function($table){
-            $table->dropColumn('email_verified_at');
-        });
+        // Schema::table('users',function($table){
+        //     $table->dropColumn('email_verified_at');
+        // });
     }
 
     /**
